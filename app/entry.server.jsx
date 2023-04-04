e9f577344e721bde280c9291e18888a4abf15797
Maulik Dodiya <maulik.dodiya@brightness-india.com>
2023-04-04 12:01:54 +0530

    first commit

--- app/entry.server.jsx
+++ app/entry.server.jsx
@@ -1,5 +1,4 @@
 import {RemixServer} from '@remix-run/react';
-import isbot from 'isbot';
 import {renderToReadableStream} from 'react-dom/server';
 
 export default async function handleRequest(
@@ -10,23 +9,12 @@ export default async function handleRequest(
 ) {
   const body = await renderToReadableStream(
     <RemixServer context={remixContext} url={request.url} />,
-    {
-      signal: request.signal,
-      onError(error) {
-        // eslint-disable-next-line no-console
-        console.error(error);
-        responseStatusCode = 500;
-      },
-    },
   );
 
-  if (isbot(request.headers.get('user-agent'))) {
-    await body.allReady;
-  }
-
   responseHeaders.set('Content-Type', 'text/html');
+
   return new Response(body, {
-    headers: responseHeaders,
     status: responseStatusCode,
+    headers: responseHeaders,
   });
 }
