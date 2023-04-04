649c11b74019e0ce11251107aa9f80ad0c4e1328
Maulik Dodiya <maulik.dodiya@brightness-india.com>
2023-04-04 16:12:16 +0530

    main four section dynamic data

--- app/components/CollectionsGrid.jsx
+++ app/components/CollectionsGrid.jsx
@@ -1,59 +1,83 @@
-import {Image} from '@shopify/hydrogen';
-import {Heading, Section, Grid, Link, ArrowRightLight} from '~/components';
+import { Image } from '@shopify/hydrogen';
+import { Heading, Section, Link, ArrowRightLight } from '~/components';
 
-export function CollectionsGrid({collections, title = '', ...props}) {
+export function CollectionsGrid({ data, ...props }) {
   return (
-    <Section {...props} heading={title} className={'my-8'}>
+    <Section {...props} className={'my-8'}>
       <div className="container">
         <div className="grid grid-rows-3 grid-flow-col gap-3">
-          {/* <div className="row-span-3">
-          <Link to={`#`}>
-            <div className="card-image bg-primary/5 aspect-[3/2]">
-              <Image
-                data={{
-                  url: 'https://cdn.shopify.com/s/files/1/0739/7172/8705/files/logo.svg?v=1680004853',
-                  width: 82,
-                  height: 36,
-                  altText: '93',
-                }}
-                className="logo-img mx-auto"
-                loaderOptions={{
-                  scale: 2,
-                  crop: 'center',
-                }}
-                alt="93"
-              />
-            </div>
-            <Heading size="copy">asdadsa</Heading>
-          </Link>
-        </div> */}
           <div className="row-span-4 bg-gray-200">
-            <Link to={`#`}>
+            <Link to={data?.section_1_button_redirect?.value}>
               <div className="relative">
                 <Image
-                  data={{
-                    url: 'https://cdn.shopify.com/s/files/1/0742/9688/5569/files/Screenshot_173.png?v=1680600063',
-                    width: 82,
-                    height: 36,
-                    altText: '93',
-                  }}
+                  data={data?.section_1_image?.reference?.image}
                   className="logo-img mx-auto"
                   loaderOptions={{
                     scale: 2,
                     crop: 'center',
                   }}
-                  alt="93"
                 />
               </div>
               <Heading className="flex gap-2 items-center rounded-lg bg-white hover:bg-gray-50 shadow-lg p-3 max-w-xs font-normal text-base">
                 <ArrowRightLight className={'w-5 h-5'} />
-                Producte
+                {data?.section_1_button_text?.value}
               </Heading>
             </Link>
           </div>
-          <div className="row-span-4 bg-gray-200">02</div>
-          <div className="row-span-2 col-span-2 bg-gray-200">03</div>
-          <div className="row-span-2 col-span-2 bg-gray-200">04</div>
+          <div className="row-span-4 bg-gray-200">
+            <Link to={data?.section_2_button_redirect?.value}>
+              <div className="relative">
+                <Image
+                  data={data?.section_2_image?.reference?.image}
+                  className="logo-img mx-auto"
+                  loaderOptions={{
+                    scale: 2,
+                    crop: 'center',
+                  }}
+                />
+              </div>
+              <Heading className="flex gap-2 items-center rounded-lg bg-white hover:bg-gray-50 shadow-lg p-3 max-w-xs font-normal text-base">
+                <ArrowRightLight className={'w-5 h-5'} />
+                {data?.section_2_button_text?.value}
+              </Heading>
+            </Link>
+          </div>
+          <div className="row-span-2 col-span-2 bg-gray-200">
+            <Link to={data?.section_3_button_redirect?.value}>
+              <div className="relative">
+                <Image
+                  data={data?.section_3_image?.reference?.image}
+                  className="logo-img mx-auto"
+                  loaderOptions={{
+                    scale: 2,
+                    crop: 'center',
+                  }}
+                />
+              </div>
+              <Heading className="flex gap-2 items-center rounded-lg bg-white hover:bg-gray-50 shadow-lg p-3 max-w-xs font-normal text-base">
+                <ArrowRightLight className={'w-5 h-5'} />
+                {data?.section_3_button_text?.value}
+              </Heading>
+            </Link>
+          </div>
+          <div className="row-span-2 col-span-2 bg-gray-200">
+            <Link to={data?.section_4_button_redirect?.value}>
+              <div className="relative">
+                <Image
+                  data={data?.section_4_image?.reference?.image}
+                  className="logo-img mx-auto"
+                  loaderOptions={{
+                    scale: 2,
+                    crop: 'center',
+                  }}
+                />
+              </div>
+              <Heading className="flex gap-2 items-center rounded-lg bg-white hover:bg-gray-50 shadow-lg p-3 max-w-xs font-normal text-base">
+                <ArrowRightLight className={'w-5 h-5'} />
+                {data?.section_4_button_text?.value}
+              </Heading>
+            </Link>
+          </div>
         </div>
       </div>
     </Section>
