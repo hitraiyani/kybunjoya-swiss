f4893c8c198e71209486b2fdb6a025788ec9959b
Ravipatelbg <114903445+Ravipatelbg@users.noreply.github.com>
2023-04-06 16:11:09 +0530

    code Update for Collection grid

--- app/components/CollectionsGrid.jsx
+++ app/components/CollectionsGrid.jsx
@@ -3,76 +3,153 @@ import {Heading, Section, Grid, Link, ArrowRightLight} from '~/components';
 
 export function CollectionsGrid({collections, title = '', ...props}) {
   return (
-    <Section {...props} heading={title} className={'my-8 collectionsGrid-sec'}>
+    <Section {...props} heading={title} className={'collectionsGrid-sec'}>
       <div className="container">
         <div className="md:grid md:grid-rows-2 xl:grid-rows-2 md:grid-flow-col gap-7 flex flex-col">
           <div className="collectionsGrid-item relative">
-            <Link to={`#`} className="relative block w-full h-96 overflow-hidden">
-              <div className="img-wrap">
+            <Link to={`#`} className="relative block w-full overflow-hidden">
+              <div className="img-wrap w-full h-full absolute inset-0">
                 <Image
                   data={{
-                    url: 'https://cdn.shopify.com/s/files/1/0742/9688/5569/files/Screenshot_173.png?v=1680600063'
+                    url: 'https://cdn.shopify.com/s/files/1/0742/9688/5569/files/204_01031-weather_jacket-ss23-stratosphere_pearl-w-4x5-c-g4_png.png?v=1680769903',
                   }}
-                  className="h-full w-full absolute inset-0 hover:scale-110 transition hover:duration-500 object-cover object-center"
+                  className="h-full w-full absolute inset-0 transition hover:duration-500 object-cover object-center"
                   alt=""
                 />
               </div>
-              <Heading className="flex gap-2 items-center rounded-lg bg-white hover:bg-gray-50 shadow-lg p-3 max-w-xs font-normal text-base absolute bottom-5 left-5 btn">
-                <ArrowRightLight className={'w-5 h-5'} />
-                Produkte
+              <div className="img-overlay absolute w-full h-2/4 inset-x-0 bottom-0"></div>
+              <Heading className="flex flex-col font-normal text-[30px] lg:text-[34px] xl:text-[45px] text-white absolute bottom-0 left-0 p-5 btn gap-5 leading-none">
+                <span>Produkte</span>
+                <ArrowRightLight
+                  className={
+                    'w-12 h-12 border rounded-full p-2 text-white border-white transition-all'
+                  }
+                />
               </Heading>
             </Link>
           </div>
           <div className="collectionsGrid-item relative">
-            <Link to={`#`} className="relative block w-full h-96 overflow-hidden">
-              <div className="img-wrap">
+            <Link to={`#`} className="relative block w-full overflow-hidden">
+              <div className="img-wrap w-full h-full absolute inset-0">
                 <Image
                   data={{
-                    url: 'https://cdn.shopify.com/s/files/1/0742/9688/5569/files/Screenshot_174.png?v=1680600063'
+                    url: 'https://cdn.shopify.com/s/files/1/0742/9688/5569/files/homepage-shop_card-apparel-ss23-1_jpg.png?v=1680769904',
                   }}
-                  className="h-full w-full absolute inset-0 hover:scale-110 transition hover:duration-500 object-cover object-center"
+                  className="h-full w-full absolute inset-0 transition hover:duration-500 object-cover object-center"
                   alt=""
                 />
               </div>
-              <Heading className="flex gap-2 items-center rounded-lg bg-white hover:bg-gray-50 shadow-lg p-3 max-w-xs font-normal text-base absolute bottom-5 left-5 btn">
-                <ArrowRightLight className={'w-5 h-5'} />
-                Verkaufsstellen
+              <div className="img-overlay absolute w-full h-2/4 inset-x-0 bottom-0"></div>
+              <Heading className="flex flex-col font-normal text-[30px] lg:text-[34px] xl:text-[45px] text-white absolute bottom-0 left-0 p-5 btn gap-5 leading-none">
+                <span>Verkaufstellen</span>
+                <ArrowRightLight
+                  className={
+                    'w-12 h-12 border rounded-full p-2 text-white border-white transition-all'
+                  }
+                />
               </Heading>
             </Link>
           </div>
           <div className="collectionsGrid-item relative">
-            <Link to={`#`} className="relative block w-full h-full overflow-hidden">
-              <div className="img-wrap"> 
+            <Link
+              to={`#`}
+              className="relative block w-full h-full overflow-hidden"
+            >
+              <div className="img-wrap w-full h-full absolute inset-0">
                 <Image
                   data={{
-                    url: 'https://cdn.shopify.com/s/files/1/0742/9688/5569/files/Screenshot_176.png?v=1680604329'
+                    url: 'https://cdn.shopify.com/s/files/1/0742/9688/5569/files/SS22_Ultralight_Mid_Sock_WhiteBlack_MW_Editorial_03_jpg.png?v=1680769903',
                   }}
-                  className="h-full w-full absolute inset-0 hover:scale-110 transition hover:duration-500 object-cover object-center"
+                  className="h-full w-full absolute inset-0 transition hover:duration-500 object-cover object-center"
                   alt=""
                 />
               </div>
-              <Heading className="flex gap-2 items-center rounded-lg bg-white hover:bg-gray-50 shadow-lg p-3 max-w-xs font-normal text-base absolute bottom-5 left-5 btn">
-                <ArrowRightLight className={'w-5 h-5'} />
-                Ihr Ratgeber
+              <div className="img-overlay absolute w-full h-2/4 inset-x-0 bottom-0"></div>
+              <Heading className="flex flex-col font-normal text-[30px] lg:text-[34px] xl:text-[45px] text-white absolute bottom-0 left-0 p-5 btn gap-5 leading-none">
+                <span>
+                  Dr. kybun Joya <br />
+                  Ratgeber
+                </span>
+                <ArrowRightLight
+                  className={
+                    'w-12 h-12 border rounded-full p-2 text-white border-white transition-all'
+                  }
+                />
               </Heading>
             </Link>
           </div>
           <div className="collectionsGrid-item relative">
-            <Link to={`#`} className="relative block w-full h-full overflow-hidden">
-              <div className="img-wrap">
+            <Link
+              to={`#`}
+              className="relative block w-full h-full overflow-hidden"
+            >
+              <div className="img-wrap w-full h-full absolute inset-0">
                 <Image
                   data={{
-                    url: 'https://cdn.shopify.com/s/files/1/0742/9688/5569/files/vison_history_section.png?v=1680602992'
+                    url: 'https://cdn.shopify.com/s/files/1/0742/9688/5569/files/SS22_Ultralight_Mid_Sock_WhiteBlack_MW_Editorial_03.jpg_1.png?v=1680769903',
                   }}
-                  className="h-full w-full absolute inset-0 hover:scale-110 transition hover:duration-500 object-cover object-center"
+                  className="h-full w-full absolute inset-0 transition hover:duration-500 object-cover object-center"
                   alt=""
                 />
               </div>
-              <Heading className="flex gap-2 items-center rounded-lg bg-white hover:bg-gray-50 shadow-lg p-3 max-w-xs font-normal text-base absolute bottom-5 left-5 btn">
-                <ArrowRightLight className={'w-5 h-5'} />
-                Vision / History
+              <div className="img-overlay absolute w-full h-2/4 inset-x-0 bottom-0"></div>
+              <div className="flex absolute bottom-0 left-0 p-5 w-full items-end h-full">
+                <div className="content-img relative overflow-hidden w-2/4 h-full">
+                  <Image
+                    data={{
+                      url: 'https://cdn.shopify.com/s/files/1/0742/9688/5569/files/Mask_group.png?v=1680773998',
+                    }}
+                    className="h-full w-full object-cover object-center"
+                    alt=""
+                  />
+                </div>
+                <Heading className="flex flex-col font-normal text-[30px] lg:text-[34px] xl:text-[45px] text-white leading-none p-5 btn w-2/4 gap-5">
+                  <span>Vision & Story</span>
+                  <ArrowRightLight
+                    className={
+                      'w-12 h-12 border rounded-full p-2 text-white border-white transition-all'
+                    }
+                  />
+                </Heading>
+              </div>
+            </Link>
+          </div>
+        </div>
+        <div className='quick-links-sec bg-[#EDEDED] mt-[28px]'>
+        <div className="collectionsGrid-item flex flex-wrap">
+            <Link to={`#`} className="relative block overflow-hidden w-full lg:w-3/5">
+              <div className="img-wrap w-full h-full absolute inset-0">
+                <Image
+                  data={{
+                    url: 'https://cdn.shopify.com/s/files/1/0742/9688/5569/files/SS22_Cloudmonster_Mood_Acai_Lavender_2_jpg.png?v=1680769904',
+                  }}
+                  className="h-full w-full absolute inset-0 transition hover:duration-500 object-cover object-center"
+                  alt=""
+                />
+              </div>
+              <div className="img-overlay absolute w-full h-2/4 inset-x-0 bottom-0"></div>
+              <Heading className="flex flex-col font-normal text-[30px] lg:text-[34px] xl:text-[45px] text-white absolute bottom-0 left-0 p-5 btn gap-5 leading-none">
+                <span>Produkte</span>
+                <ArrowRightLight
+                  className={
+                    'w-12 h-12 border rounded-full p-2 text-white border-white transition-all'
+                  }
+                />
               </Heading>
             </Link>
+            <div className='quick-link-wrap px-[20px] py-[30px] lg:px-[49px] lg:py-[30px] w-full lg:w-2/5'>
+              <h2 className='title text-[#595959] text-[25px] md:mb-[40px] xl:mb-[83px] font-medium'>Quick Links</h2>
+              <div className='quick-link-list'>
+                <ul>
+                  <li><a href="" className='text-[30px] md:text-[30px] lg:text-[34px] xl:text-[50px] text-black hover:text[#00795C] block tracking-[-0.97152px]'>Careers</a></li>
+                  <li><a href="" className='text-[30px] md:text-[30px] lg:text-[34px] xl:text-[50px] text-black hover:text[#00795C] block tracking-[-0.97152px]'>Schweizer Schuhproduktion</a></li>
+                  <li><a href="" className='text-[30px] md:text-[30px] lg:text-[34px] xl:text-[50px] text-black hover:text[#00795C] block tracking-[-0.97152px]'>Luftkissen Technologie</a></li>
+                  <li><a href="" className='text-[30px] md:text-[30px] lg:text-[34px] xl:text-[50px] text-black hover:text[#00795C] block tracking-[-0.97152px]'>Sustainability</a></li>
+                  <li><a href="" className='text-[30px] md:text-[30px] lg:text-[34px] xl:text-[50px] text-black hover:text[#00795C] block tracking-[-0.97152px]'>Brands</a></li>
+                  <li><a href="" className='text-[30px] md:text-[30px] lg:text-[34px] xl:text-[50px] text-black hover:text[#00795C] block tracking-[-0.97152px]'>Partner Werden</a></li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
