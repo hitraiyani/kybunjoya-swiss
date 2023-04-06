872556b2412ecf8f6aa3bb39d7aadc85290ec2cc
pradip.m <p.maurya@brightness-india.com>
2023-04-06 17:36:41 +0530

    Article section design

--- app/components/ArticleSlider.jsx
+++ app/components/ArticleSlider.jsx
@@ -10,19 +10,20 @@ export function ArticleSlider({ articles }) {
     return (
         <section>
             <div className="container">
-                <h3 className='text-[#00795C] w-full font-bold text-2xl mb-3'>Neues und Aktuelles</h3>   
+                <h3 className='text-[#00795C] w-full font-bold text-[35px] lg:text-[55px]  mb-3 mt-16'>Neues und Aktuelles</h3>   
                 <Swiper
                     modules={[Navigation, Scrollbar, A11y, Autoplay]}
                     slidesPerView={3}
                     navigation
                     loop="true"
+                    spaceBetween= "16"
                     // autoplay={{
                     //   delay: 2500,
                     //   disableOnInteraction: false,
                     // }}
-                    autoplay="false"
+                    autoplay="false" className='mb-8'
                 >
-                    <div className="grid grid-cols-3">
+                    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
                         {articles?.edges?.map((article, index) => {
                             return (
                                 <SwiperSlide key={index}>
@@ -30,14 +31,17 @@ export function ArticleSlider({ articles }) {
                                         <Link to={`#`} className="relative block overflow-hidden">
                                         <div className="img-wrap">
                                         <img
-                                                className="object-cover object-center"
+                                                className="object-cover object-center w-full max-h-[573px] h-[573px] xl:max-h-[573px] xl:h-[573px] lg:max-h-[300px] lg:h-[300px] md:max-h-[300px] md:h-[300px] rounded-md	drop-shadow-md"
                                                 src={article.node.image.url}
                                             ></img>
                                         </div>
                                         </Link>
                                     </div>
-                                    <p>{article.node.title}</p><br/>
-                                    <p>{article.node.content}</p>
+                                    <div className='max-w-[85%]'>
+                                        <p className=' text-[24px] text-black lg:text-[35px] w-full font-bold'>{article.node.title}</p>
+                                        <p  className=' text-[14px]  text-black lg:text-[15px] w-full font-normal'>{article.node.content}</p>
+                                    </div>
+                                   
                                 </SwiperSlide>
                             
                             );
