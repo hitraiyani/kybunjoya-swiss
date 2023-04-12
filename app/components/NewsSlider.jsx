ac22c1ec78f32f6e308aa42f90aa0ff2a38a309e
Ravipatelbg <114903445+Ravipatelbg@users.noreply.github.com>
2023-04-12 13:22:19 +0530

    push the code for news detail page.

--- app/components/NewsSlider.jsx
+++ app/components/NewsSlider.jsx
@@ -1,81 +1,93 @@
-import { Link, Heading } from '~/components';
-import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { toHTML, truncate } from '~/lib/utils';
-import { STORE_LOCALE} from '~/lib/const';
+import {Link, Heading} from '~/components';
+import {Navigation, Pagination, Scrollbar, A11y, Autoplay} from 'swiper';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {toHTML, truncate} from '~/lib/utils';
+import {STORE_LOCALE} from '~/lib/const';
 
 /**
  * Hero component that renders metafields attached to collection resources
  **/
-export function NewsSlider({ news }) {
-    return (
-        <section className='mb-16 article-slide'>
-            <div className="container">
-                <h3 className='text-[#00795C] w-full font-bold text-[35px] lg:text-[55px]  mb-3 mt-16'>Neues und Aktuelles</h3>   
-                <Swiper className="article-slider"
-                    modules={[Navigation, Scrollbar, A11y, Autoplay]}
-                    slidesPerView={3}
-                    navigation
-                    loop="true"
-                    spaceBetween= "16"
-                    // autoplay={{
-                    //   delay: 2500,
-                    //   disableOnInteraction: false,
-                    // }}
-                    autoplay="false" 
-                    breakpoints={{
-                        0: {
-                         slidesPerView: 1.3,
-                         spaceBetween: 10,
-                       },
-                       768: {
-                         slidesPerView: 3,
-                         spaceBetween: 20,
-                       },
-                     }}
-                >
-                    <div className="grid grid-cols-1 gap-4 mb-8 md:grid-cols-2 xl:grid-cols-3">
-                        {news?.map((newItem, index) => {
-                            let imgSrc = newItem?.attributes?.optimizedImage;
-                            if(newItem.attributes.optimizedImage == null){
-                                imgSrc = newItem.attributes.image;
-                            }else if(newItem.attributes.image == null){
-                                imgSrc = '';
-                            }
-                            var catName = '';
-                            if( newItem?.attributes?.newsCategory != null ){
-                                var cat =  newItem?.attributes?.newsCategory;
-                                for(var nc=0; nc<cat.translations.length; nc++){
-                                  if(cat.translations[nc].locale==STORE_LOCALE){
-                                    var catName = cat.translations[nc].webName ? cat.translations[nc].webName : cat.translations[nc].name;
-                                  }
-                                }
-                            }
-                            return (
-                                <SwiperSlide key={index}>
-                                    <div>
-                                        <span>Brand { catName ? `/ ${catName}` : ''}</span>
-                                        <Link to={`/pages/news/${newItem.attributes.urlHandle}`} className="relative block overflow-hidden mb-5">
-                                        <div className="img-wrap">
-                                        <img
-                                                className="object-cover object-center w-full rounded-md aspect-square drop-shadow-md"
-                                                src={imgSrc}
-                                            ></img>
-                                        </div>
-                                        </Link>
-                                    </div>
-                                    <div className='max-w-[85%]'>
-                                        <p className=' text-[24px] text-black lg:text-[35px] w-full font-bold leading-[1.2] mb-[15px]'>{newItem?.attributes?.name}</p>
-                                        <p  className=' text-[14px]  text-black lg:text-[15px] w-full font-normal'>{truncate(newItem?.attributes?.excerpt)}</p>
-                                    </div>
-                                   
-                                </SwiperSlide>
-                            
-                            );
-                        })}
-                    </div>
-                </Swiper>
-            </div>
-        </section>
-    );
+export function NewsSlider({news}) {
+  return (
+    <section className="mb-16 article-slide">
+      <div className="container">
+        <h3 className="text-[#00795C] w-full font-bold text-[35px] lg:text-[55px]  mb-3 mt-16">
+          Neues und Aktuelles
+        </h3>
+        <Swiper
+          className="article-slider"
+          modules={[Navigation, Scrollbar, A11y, Autoplay]}
+          slidesPerView={3}
+          navigation
+          loop="true"
+          spaceBetween="16"
+          // autoplay={{
+          //   delay: 2500,
+          //   disableOnInteraction: false,
+          // }}
+          autoplay="false"
+          breakpoints={{
+            0: {
+              slidesPerView: 1.3,
+              spaceBetween: 10,
+            },
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+          }}
+        >
+          <div className="grid grid-cols-1 gap-4 mb-8 md:grid-cols-2 xl:grid-cols-3">
+            {news?.map((newItem, index) => {
+              let imgSrc = newItem?.attributes?.optimizedImage;
+              if (newItem.attributes.optimizedImage == null) {
+                imgSrc = newItem.attributes.image;
+              } else if (newItem.attributes.image == null) {
+                imgSrc = '';
+              }
+              var catName = '';
+              if (newItem?.attributes?.newsCategory != null) {
+                var cat = newItem?.attributes?.newsCategory;
+                for (var nc = 0; nc < cat.translations.length; nc++) {
+                  if (cat.translations[nc].locale == STORE_LOCALE) {
+                    var catName = cat.translations[nc].webName
+                      ? cat.translations[nc].webName
+                      : cat.translations[nc].name;
+                  }
+                }
+              }
+              return (
+                <SwiperSlide key={index}>
+                  <div>
+                    <Link
+                      to={`/pages/news/${newItem.attributes.urlHandle}`}
+                      className="relative block overflow-hidden mb-5"
+                    >
+                      <div className="img-wrap">
+                        <img
+                          className="object-cover object-center w-full rounded-md aspect-square drop-shadow-md"
+                          src={imgSrc}
+                        ></img>
+                      </div>
+                      <p className="text-[16px] text-white font-normal mb-[5px] bg-[#00795C] leading-none top-0 right-0 w-fit py-[10px] px-[16px] rounded-tr-md absolute shadow-md">
+                        Brand {catName ? `/ ${catName}` : ''}
+                      </p>
+                    </Link>
+                  </div>
+                  <div className="max-w-[85%]">
+                    <p className=" text-[24px] text-black lg:text-[35px] w-full font-bold leading-[1.2] mb-[15px]">
+                      {newItem?.attributes?.name}
+                    </p>
+                    <p className=" text-[14px]  text-black lg:text-[15px] w-full font-normal">
+                      {truncate(newItem?.attributes?.excerpt)}
+                    </p>
+                  </div>
+                </SwiperSlide>
+              );
+            })}
+          </div>
+        </Swiper>
+      </div>
+    </section>
+  );
 }
--- app/routes/($lang)/pages/news/$newsHandle.jsx
+++ app/routes/($lang)/pages/news/$newsHandle.jsx
@@ -1,13 +1,17 @@
 import {json} from '@shopify/remix-oxygen';
 import {useLoaderData} from '@remix-run/react';
 import {Image} from '@shopify/hydrogen';
-import { useEffect, useState  } from 'react';
+import {useEffect, useState} from 'react';
 import invariant from 'tiny-invariant';
 import {PageHeader, Section} from '~/components';
-import {AICO_API_URL, AICO_API_TOKEN, STORE_LOCALE, ATTR_LOADING_EAGER} from '~/lib/const';
+import {
+  AICO_API_URL,
+  AICO_API_TOKEN,
+  STORE_LOCALE,
+  ATTR_LOADING_EAGER,
+} from '~/lib/const';
 import {newsDescription} from '~/lib/utils';
 import styles from '~/styles/news.css';
-
 
 // const seo = ({data}) => ({
 //   title: data?.article?.seo?.title,
@@ -24,13 +28,15 @@ export async function loader({params, context}) {
 
   invariant(params.newsHandle, 'Missing journal handle');
 
-
-  const newsResponse = await fetch(`${AICO_API_URL}news?filter[urlHandle]=${params.newsHandle}`, {
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${AICO_API_TOKEN}`,
-    }
-  });
+  const newsResponse = await fetch(
+    `${AICO_API_URL}news?filter[urlHandle]=${params.newsHandle}`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${AICO_API_TOKEN}`,
+      },
+    },
+  );
 
   const news = await newsResponse.json();
 
@@ -39,12 +45,14 @@ export async function loader({params, context}) {
   }
   const newsObj = news?.data[0]?.attributes;
 
-
-   newsObj.formattedpublishDate = new Intl.DateTimeFormat(`${language}-${country}`, {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  }).format(new Date(newsObj?.publishDate));
+  newsObj.formattedpublishDate = new Intl.DateTimeFormat(
+    `${language}-${country}`,
+    {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    },
+  ).format(new Date(newsObj?.publishDate));
 
   return json(
     {newsObj},
@@ -67,7 +75,13 @@ export const handle = {
 };
 
 export const links = () => {
-    return [{rel: 'stylesheet', href: styles}, {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css'}];
+  return [
+    {rel: 'stylesheet', href: styles},
+    {
+      rel: 'stylesheet',
+      href: 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css',
+    },
+  ];
 };
 
 export default function Article() {
@@ -77,13 +91,15 @@ export default function Article() {
   const newsContent = newsDescription(newsObj?.contentBuilder);
 
   var catName = '';
-  if( newsObj.newsCategory != null ){
-      var cat =  newsObj.newsCategory;
-      for(var nc=0; nc<cat.translations.length; nc++){
-        if(cat.translations[nc].locale==STORE_LOCALE){
-          var catName = cat.translations[nc].webName ? cat.translations[nc].webName : cat.translations[nc].name;
-        }
+  if (newsObj.newsCategory != null) {
+    var cat = newsObj.newsCategory;
+    for (var nc = 0; nc < cat.translations.length; nc++) {
+      if (cat.translations[nc].locale == STORE_LOCALE) {
+        var catName = cat.translations[nc].webName
+          ? cat.translations[nc].webName
+          : cat.translations[nc].name;
       }
+    }
   }
 
   useEffect(() => {
@@ -92,18 +108,19 @@ export default function Article() {
     scriptJquery.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
     scriptJquery.async = true;
     scriptJquery.onload = () => {
-      setScriptsLoaded(prevState => ({ ...prevState, jqueryLoaded: true }));
+      setScriptsLoaded((prevState) => ({...prevState, jqueryLoaded: true}));
 
-       // Load Slick script
+      // Load Slick script
       const scriptSlick = document.createElement('script');
-      scriptSlick.src = 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js';
+      scriptSlick.src =
+        'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js';
       scriptSlick.async = true;
       scriptSlick.onload = () => {
-        setScriptsLoaded(prevState => ({ ...prevState, slickLoaded: true }));
+        setScriptsLoaded((prevState) => ({...prevState, slickLoaded: true}));
       };
       document.body.appendChild(scriptSlick);
-  };
-  document.body.appendChild(scriptJquery);
+    };
+    document.body.appendChild(scriptJquery);
 
     return () => {
       // Clean up the script tags when the component unmounts
@@ -114,74 +131,82 @@ export default function Article() {
 
   useEffect(() => {
     if (scriptsLoaded?.jqueryLoaded && scriptsLoaded?.slickLoaded) {
-        // Initialize Slick slider
-        setTimeout(() => {   
-            $('.slider-for').slick({
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: false,
-                //arrows: true,
-                asNavFor: '.slider-nav',
-                adaptiveHeight: true
-            });
-            $('.slider-nav').slick({
+      // Initialize Slick slider
+      setTimeout(() => {
+        $('.slider-for').slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          //arrows: true,
+          asNavFor: '.slider-nav',
+          adaptiveHeight: true,
+        });
+        $('.slider-nav').slick({
+          slidesToShow: 7,
+          slidesToScroll: 1,
+          asNavFor: '.slider-for',
+          centerMode: true,
+          focusOnSelect: true,
+          infinite: false,
+          responsive: [
+            {
+              breakpoint: 767,
+              settings: {
                 slidesToShow: 7,
-                slidesToScroll: 1,
-                asNavFor: '.slider-for',
-                centerMode: true,
-                focusOnSelect: true,
-                infinite: false,
-                responsive: [
-                {
-                    breakpoint: 767,
-                    settings: {
-                        slidesToShow: 7,
-                    }
-                },
-                {
-                    breakpoint: 540,
-                    settings: {
-                        slidesToShow: 5,
-                    }
-                }
-                ]
-            });
-        },300)
+              },
+            },
+            {
+              breakpoint: 540,
+              settings: {
+                slidesToShow: 5,
+              },
+            },
+          ],
+        });
+      }, 300);
     }
-  },[scriptsLoaded])
+  }, [scriptsLoaded]);
 
   return (
     <>
-          <div className="container">
-              <div className="title-wrap mt-[85px] lg:mt-[85px]">
+      <div className="container mx-auto">
+        <div className="title-wrap mt-[85px] lg:mt-[85px]"></div>
+      </div>
+      <div className="container mx-auto">
+        <div id="news-details-container">
+          <div className="news-content-wrapper">
+            <div className="flex flex-wrap gap-2 text-[16px] text-black">
+              <span>Brand {catName ? `/ ${catName}` : ''}</span>
+              <span>
+                {newsObj?.formattedpublishDate}
+              </span>
+            </div>
+            {newsObj.image && (
+              <div className='new--main-img img-wrap relative overflow-hidden rounded-md pb-[30%] mt-[20px] min-h-[350px]'>
+                <Image
+                  data={{
+                    url: newsObj.image,
+                  }}
+                  className="rounded-md absolute inset-0 w-full h-full object-cover"
+                  widths={[400, 800, 1200]}
+                  width="100px"
+                  loading={ATTR_LOADING_EAGER}
+                  loaderOptions={{
+                    scale: 2,
+                    crop: 'center',
+                  }}
+                />
               </div>
-          </div>
-          <div id="news-details-container">
-              <div className="news-content-wrapper">
-                <span>Brand { catName ? `/ ${catName}` : ''}</span><span className="float-right">{newsObj?.formattedpublishDate}</span>
-                {newsObj.image && (
-                    <Image
-                        data={{
-                            url: newsObj.image,
-                        }}
-                        className="w-full mx-auto mt-8 md:mt-16 max-w-7xl h-60"
-                        widths={[400, 800, 1200]}
-                        width="100px"
-                        loading={ATTR_LOADING_EAGER}
-                        loaderOptions={{
-                        scale: 2,
-                        crop: 'center',
-                        }}
-                    />
-                    )}
-                  <div className="page-width page-width--narrow">
-                      <div className="title-wrapper">
-                          <h2>{newsObj?.name}</h2>
-                      </div>
-                      <div dangerouslySetInnerHTML={{__html: newsContent }} />
-                  </div>
+            )}
+            <div className="page-width page-width--narrow">
+              <div className="title-wrapper">
+                <h2>{newsObj?.name}</h2>
               </div>
+              <div dangerouslySetInnerHTML={{__html: newsContent}} />
+            </div>
           </div>
+        </div>
+      </div>
     </>
   );
 }
