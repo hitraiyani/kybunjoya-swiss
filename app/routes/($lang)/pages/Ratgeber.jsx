af05d6c80a8e63b979f16e8ab15d686a2ae5883d
Maulik Dodiya <maulik.dodiya@brightness-india.com>
2023-04-18 16:39:37 +0530

    Ratgeber Page Dynamic Changes

--- app/routes/($lang)/pages/Ratgeber.jsx
+++ app/routes/($lang)/pages/Ratgeber.jsx
@@ -1,36 +1,73 @@
-import {ArrowRightLight} from '~/components';
+import React from 'react';
+import {json} from '@shopify/remix-oxygen';
+import {useLoaderData} from '@remix-run/react';
+import {MEDIA_FRAGMENT} from '~/data/fragments';
+import { toHTML} from '~/lib/utils';
+import {Link, ArrowRightLight} from '~/components';
+
+const seo = ({data}) => ({
+  title: data?.page?.seo?.title,
+  description: data?.page?.seo?.description,
+});
+
+export const handle = {
+  seo,
+};
+
+export async function loader({request, params, context}) {
+  
+  const {page} = await context.storefront.query(PAGE_QUERY, {
+    variables: {
+      handle: 'ratgeber',
+      language: context.storefront.i18n.language,
+    },
+  });
+
+  if (!page) {
+    throw new Response(null, {status: 404});
+  }
+
+  return json(
+    {page},
+    {
+      headers: {
+        // TODO cacheLong()
+      },
+    },
+  );
+}
 
 export default function ratgeber() {
+  const {page} = useLoaderData();
+
+  console.log("page", (page.ratgeber.reference));
+
+
   return (
     <>
       <div className="container mt-[200px]">
         <section className="page-title">
           <h1 className="text-[#00795C] text-[35px] lg:text-[40px] xl:text-[65px] tracking-[-0.97152px] mb-6">
-            Ratgeber
+            {page?.title}
           </h1>
         </section>
         <section className="hero-banner-section ratgeber-banner-section rounded-xl overflow-hidden">
           <div className="hero-banner-inner relative">
-            <div className="banner-info">
-              <div className="flex flex-wrap items-end px-[72px] py-[57px] justify-between">
-                <div className="title-wrap">
-                  <h2 className="text-white text-[35px] lg:text-[40px] xl:text-[55px] tracking-[-0.97152px] leading-[1.2]">
-                    Therapieren
-                    <br /> statt operieren
-                  </h2>
+            <div className='banner-info'>
+              <div className='flex flex-wrap items-end px-[72px] py-[57px] justify-between'>
+                <div className='title-wrap'>
+                  <h2 className='text-white text-[35px] lg:text-[40px] xl:text-[55px] tracking-[-0.97152px] leading-[1.1]'
+                  dangerouslySetInnerHTML={{
+                    __html: toHTML(page?.ratgeber?.reference?.hero_banner_title?.value),
+                  }}
+                  ></h2>
                 </div>
-                <div className="dr-info flex gap-[20px] items-start">
-                  <div className="name w-[280px] text-right">
-                    <h4 className="font-black text-white text-[100px] leading-[1]">
-                      Dr. kybun Joya
-                    </h4>
+                <div className='dr-info flex gap-[20px] items-start'>
+                  <div className='name w-[280px] text-right'>
+                    <h4 className='font-black text-white text-[100px] leading-[1.1]'>{page?.ratgeber?.reference?.hero_inner_banner_title?.value}</h4>
                   </div>
-                  <div className="dr-img">
-                    <img
-                      className="rounded-xl"
-                      src="https://cdn.shopify.com/s/files/1/0742/9688/5569/files/photo-yzYAazAJs_1.jpg?v=1681808468"
-                      alt=""
-                    />
+                  <div className='dr-img'>
+                    <img className='rounded-xl' src={page?.ratgeber?.reference?.hero_inner_banner?.reference?.image?.url} alt="" />
                   </div>
                 </div>
               </div>
@@ -39,38 +76,30 @@ export default function ratgeber() {
               <div className="absolute inset-x-0 bottom-0 w-full img-overlay h-2/4"></div>
               <img
                 className="w-full h-full object-cover rounded-xl"
-                src="https://cdn.shopify.com/s/files/1/0742/9688/5569/files/Picture_1_png.jpg?v=1680771114"
+                src={page?.ratgeber?.reference?.hero_banner?.reference?.image?.url}
                 alt=""
               />
             </div>
           </div>
         </section>
-        <section className="about-sec mt-[105px]">
-          <div className="about-sec-inner flex flex-col lg:flex-row gap-y-[20px] gap-x-[30px] xl:gap-x-[50px]">
-            <div className="img-col w-[50%]">
-              <img
-                className="w-full rounded-xl"
-                src="https://cdn.shopify.com/s/files/1/0742/9688/5569/files/jeremy-lapak-CVvFVQ_-oUg-unsplash_1.jpg?v=1681809606"
-                alt=""
-              />
+        <section className='about-sec mt-[105px]'>
+          <div className='about-sec-inner flex flex-col lg:flex-row gap-y-[20px] gap-x-[30px] xl:gap-x-[50px]'>
+            <div className='img-col w-[50%]'>
+              <img className='w-full rounded-xl' src={page?.ratgeber?.reference?.start_consultation_image?.reference?.image?.url} alt="" />
             </div>
-            <div className="content-info w-[50%]">
-              <h4 className="text-[45px] tracking-[-0.97152px] font-medium leading-[1.1]">
-                <span className="text-[#00795c]">
-                  «Therapieren statt operieren»
-                </span>
-                ist unsere Mission. Welche Schmerzen plagen Sie? Erfahren und
-                erleben Sie, wie die
-                <span className="text-[#00795c]">kybun Joya Therapie</span> Ihre
-                Gesundheit unterstützt und fördert.
-              </h4>
-              <a
-                href="#"
-                className="inline-block rounded-[100px] bg-black text-white
-                 text-center px-[59px] py-[25px] hover:bg-[#00795c] hover:text-white text-[18px] max-w-fit mt-[26px] leading-none"
+            <div className='content-info w-[50%]'>
+              <h4 className='text-[45px] tracking-[-0.97152px] font-medium leading-[1.1]'
+                  dangerouslySetInnerHTML={{
+                    __html: toHTML(page?.ratgeber?.reference?.start_consultation_text?.value),
+                  }}
+              ></h4>
+              <Link
+                className='inline-block rounded-[100px] bg-black text-white
+              text-center px-[59px] py-[25px] hover:bg-[#00795c] hover:text-white text-[18px] max-w-fit mt-[26px] leading-none'
+                to={page?.ratgeber?.reference?.start_consultation_button_redirect?.value}
               >
-                Beratung Starten
-              </a>
+                {page?.ratgeber?.reference?.start_consultation_button_text?.value}
+              </Link>
             </div>
           </div>
         </section>
@@ -80,81 +109,101 @@ export default function ratgeber() {
               <div className="item flex flex-col rounded-[10px]">
                 <div className="title-wrap bg-[#00795C] rounded-tl-[10px] rounded-tr-[10px]">
                   <h4 className="text-white text-[35px] font-bold leading-none p-[20px]">
-                    Medizin
+                    {page?.ratgeber?.reference?.footer_section_title_1?.value}
                   </h4>
                 </div>
                 <div className="content-info px-[20px] pt-[12px] pb-[20px] bg-[#EDEDED] rounded-bl-[10px] rounded-br-[10px] flex flex-col gap-[8px] h-full">
-                  <div className="desc text-[25px] tracking-[-0.400697px] font-normal leading-[1.4]">
-                    <p>Medizinische Themen <br/>im Überblick</p>
+                  <div className="desc text-[25px] tracking-[-0.400697px] font-normal leading-[1.4]"
+                    dangerouslySetInnerHTML={{
+                      __html: toHTML(page?.ratgeber?.reference?.footer_section_content_1?.value),
+                    }}
+                  >
                   </div>
-                  <a href="#" className='text-[#00795C] flex justify-end items-center gap-[8px] text-[25px] tracking-[-0.400697px] font-normal  mt-auto hover:!text-black'>
-                    Mehr erfahren
-                    <ArrowRightLight
-                      className={
-                        'w-[30px] h-[30px]'  
-                      }
-                    />
-                  </a>
-                </div>
-              </div>
-              <div className="item flex flex-col rounded-[10px]">
-                <div className="title-wrap bg-[#00795C] rounded-tl-[10px] rounded-tr-[10px]">
-                  <h4 className="text-white text-[35px] font-bold leading-none p-[20px]">
-                  Wirkungsweise
-                  </h4>
-                </div>
-                <div className="content-info px-[20px] pt-[12px] pb-[20px] bg-[#EDEDED] rounded-bl-[10px] rounded-br-[10px] flex flex-col gap-[8px] h-full">
-                  <div className="desc text-[25px] tracking-[-0.400697px] font-normal leading-[1.4]">
-                    <p>Wie kann das Gehen und Stehen auf weich-elastisch-federndem Material Ihre Gesundheit unterstützen?</p>
-                  </div>
-                  <a href="#" className='text-[#00795C] flex justify-end items-center gap-[8px] text-[25px] tracking-[-0.400697px] font-normal  mt-auto hover:!text-black'>
+                  <Link
+                    to={page?.ratgeber?.reference?.footer_section_redirect_1?.value}
+                    className='text-[#00795C] flex justify-end items-center gap-[8px] text-[25px] tracking-[-0.400697px] font-normal  mt-auto hover:!text-black'>
                     Mehr erfahren
                     <ArrowRightLight
                       className={
                         'w-[30px] h-[30px]'
                       }
                     />
-                  </a>
+                  </Link>
                 </div>
               </div>
               <div className="item flex flex-col rounded-[10px]">
                 <div className="title-wrap bg-[#00795C] rounded-tl-[10px] rounded-tr-[10px]">
                   <h4 className="text-white text-[35px] font-bold leading-none p-[20px]">
-                  Gesundheit-Events
+                    {page?.ratgeber?.reference?.footer_section_title_2?.value}
                   </h4>
                 </div>
                 <div className="content-info px-[20px] pt-[12px] pb-[20px] bg-[#EDEDED] rounded-bl-[10px] rounded-br-[10px] flex flex-col gap-[8px] h-full">
-                  <div className="desc text-[25px] tracking-[-0.400697px] font-normal leading-[1.4]">
-                    <p>Hier erfahren Sie, wann der nächste Gesundheits-event in ihrer Nähe stattfindet.</p>
+                  <div className="desc text-[25px] tracking-[-0.400697px] font-normal leading-[1.4]"
+                    dangerouslySetInnerHTML={{
+                      __html: toHTML(page?.ratgeber?.reference?.footer_section_content_2?.value),
+                    }}
+                  >
                   </div>
-                  <a href="#" className='text-[#00795C] flex justify-end items-center gap-[8px] text-[25px] tracking-[-0.400697px] font-normal  mt-auto hover:!text-black'>
+                  <Link
+                    to={page?.ratgeber?.reference?.footer_section_redirect_2?.value}
+                    className='text-[#00795C] flex justify-end items-center gap-[8px] text-[25px] tracking-[-0.400697px] font-normal  mt-auto hover:!text-black'>
                     Mehr erfahren
                     <ArrowRightLight
                       className={
                         'w-[30px] h-[30px]'
                       }
                     />
-                  </a>
+                  </Link>
                 </div>
               </div>
               <div className="item flex flex-col rounded-[10px]">
                 <div className="title-wrap bg-[#00795C] rounded-tl-[10px] rounded-tr-[10px]">
                   <h4 className="text-white text-[35px] font-bold leading-none p-[20px]">
-                  Studien
+                  {page?.ratgeber?.reference?.footer_section_title_3?.value}
                   </h4>
                 </div>
                 <div className="content-info px-[20px] pt-[12px] pb-[20px] bg-[#EDEDED] rounded-bl-[10px] rounded-br-[10px] flex flex-col gap-[8px] h-full">
-                  <div className="desc text-[25px] tracking-[-0.400697px] font-normal leading-[1.4]">
-                    <p>Erhalten Sie Hintergrundinfos zu unseren Studien und Gütersiegeln.</p>
+                  <div className="desc text-[25px] tracking-[-0.400697px] font-normal leading-[1.4]"
+                    dangerouslySetInnerHTML={{
+                      __html: toHTML(page?.ratgeber?.reference?.footer_section_content_3?.value),
+                    }}
+                  >
                   </div>
-                  <a href="#" className='text-[#00795C] flex justify-end items-center gap-[8px] text-[25px] tracking-[-0.400697px] font-normal  mt-auto hover:!text-black'>
+                  <Link
+                    to={page?.ratgeber?.reference?.footer_section_redirect_3?.value}
+                    className='text-[#00795C] flex justify-end items-center gap-[8px] text-[25px] tracking-[-0.400697px] font-normal  mt-auto hover:!text-black'>
                     Mehr erfahren
                     <ArrowRightLight
                       className={
                         'w-[30px] h-[30px]'
                       }
                     />
-                  </a>
+                  </Link>
+                </div>
+              </div>
+              <div className="item flex flex-col rounded-[10px]">
+                <div className="title-wrap bg-[#00795C] rounded-tl-[10px] rounded-tr-[10px]">
+                  <h4 className="text-white text-[35px] font-bold leading-none p-[20px]">
+                    {page?.ratgeber?.reference?.footer_section_title_4?.value}
+                  </h4>
+                </div>
+                <div className="content-info px-[20px] pt-[12px] pb-[20px] bg-[#EDEDED] rounded-bl-[10px] rounded-br-[10px] flex flex-col gap-[8px] h-full">
+                  <div className="desc text-[25px] tracking-[-0.400697px] font-normal leading-[1.4]"
+                    dangerouslySetInnerHTML={{
+                      __html: toHTML(page?.ratgeber?.reference?.footer_section_content_4?.value),
+                    }}
+                  >
+                  </div>
+                  <Link
+                    to={page?.ratgeber?.reference?.footer_section_redirect_4?.value}
+                    className='text-[#00795C] flex justify-end items-center gap-[8px] text-[25px] tracking-[-0.400697px] font-normal  mt-auto hover:!text-black'>
+                    Mehr erfahren
+                    <ArrowRightLight
+                      className={
+                        'w-[30px] h-[30px]'
+                      }
+                    />
+                  </Link>
                 </div>
               </div>
             </div>
@@ -164,3 +213,92 @@ export default function ratgeber() {
     </>
   );
 }
+
+const PAGE_QUERY = `#graphql
+${MEDIA_FRAGMENT}
+  query PageDetails($language: LanguageCode, $handle: String!)
+  @inContext(language: $language) {
+    page(handle: $handle) {
+      id
+      title
+      body
+      ratgeber : metafield(namespace: "custom", key: "ratgeber") {
+        reference {
+          ... on Metaobject {
+            handle
+            hero_banner : field(key: "hero_banner") {
+              reference {
+                ...Media
+              }
+            }
+            hero_inner_banner : field(key: "hero_inner_banner") {
+              reference {
+                ...Media
+              }
+            }
+            hero_inner_banner_title : field(key: "hero_inner_banner_title") {
+              value
+            }
+            hero_banner_title : field(key: "hero_banner_title") {
+              value
+            }
+            start_consultation_text : field(key: "start_consultation_text") {
+              value
+            }
+            start_consultation_button_text : field(key: "start_consultation_button_text") {
+              value
+            }
+            start_consultation_button_redirect : field(key: "start_consultation_button_redirect") {
+              value
+            }
+            start_consultation_image : field(key: "start_consultation_image") {
+              reference {
+                ...Media
+              }
+            }
+            footer_section_title_1 : field(key: "footer_section_title_1") {
+              value
+            }
+            footer_section_content_1 : field(key: "footer_section_content_1") {
+              value
+            }
+            footer_section_redirect_1 : field(key: "footer_section_redirect_1") {
+              value
+            }
+            footer_section_title_2 : field(key: "footer_section_title_2") {
+              value
+            }
+            footer_section_content_2 : field(key: "footer_section_content_2") {
+              value
+            }
+            footer_section_redirect_2 : field(key: "footer_section_redirect_2") {
+              value
+            }
+            footer_section_title_3 : field(key: "footer_section_title_3") {
+              value
+            }
+            footer_section_content_3 : field(key: "footer_section_content_3") {
+              value
+            }
+            footer_section_redirect_3 : field(key: "footer_section_redirect_3") {
+              value
+            }
+            footer_section_title_4 : field(key: "footer_section_title_4") {
+              value
+            }
+            footer_section_content_4 : field(key: "footer_section_content_4") {
+              value
+            }
+            footer_section_redirect_4 : field(key: "footer_section_redirect_4") {
+              value
+            }
+          }
+        }
+      }
+      seo {
+        description
+        title
+      }
+    }
+  }
+`;
