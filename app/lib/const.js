41314c95bb8a94290b187eb4c96f2325ccd287bd
Maulik Dodiya <maulik.dodiya@brightness-india.com>
2023-05-08 11:40:05 +0530

    change api AICO api endpoint

--- app/lib/const.js
+++ app/lib/const.js
@@ -1,10 +1,10 @@
 export const PAGINATION_SIZE = 8;
 export const DEFAULT_GRID_IMG_LOAD_EAGER_COUNT = 4;
 export const ATTR_LOADING_EAGER = 'eager';
-export const AICO_API_URL = 'https://app.aico.swiss/19947/api/v1/';
-//export const AICO_API_URL = 'https://kjmvp.aico.swiss/api/v1/';
+//export const AICO_API_URL = 'https://app.aico.swiss/19947/api/v1/';
+export const AICO_API_URL = 'https://kjmvp.aico.swiss/api/v1/';
 export const AICO_API_IMAGE_PREFIX = 'https://kjmvp.aico.swiss/storage/';
-export const AICO_API_TOKEN = 'bgdGl9qrG33Ice5KwRavIwklKT61faTnhyCHiQ6n';
+export const AICO_API_TOKEN = 'aJ5cIAdY16usaN37pnw5ed4GFdJhSSkrIYCxpDBK';
 export const STORE_LOCALE = 'de_CH';
 
 export function getImageLoadingPriority(
