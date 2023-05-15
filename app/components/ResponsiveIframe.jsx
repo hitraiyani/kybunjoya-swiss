c4e91f4d8fcd7fe1d61cb4467df07cb6b0a36338
Ravipatelbg <114903445+Ravipatelbg@users.noreply.github.com>
2023-05-15 19:05:19 +0530

    Push the code for blog detail page

--- app/components/ResponsiveIframe.jsx
+++ app/components/ResponsiveIframe.jsx
@@ -1,19 +1,59 @@
-import React, {useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 export function ResponsiveIframe({url}) {
-  const iframeRef = useRef();
+  const iFrameRef = useRef(null);
+  const [iframeHeight, setIframeHeight] = useState(0);
+
+  useEffect(() => {
+    function handleMessage(event) {
+      if (event.data && event.data.type === 'resizeIframe') {
+        const {height} = event.data;
+        setIframeHeight(height);
+      }
+    }
+
+    window.addEventListener('message', handleMessage);
+
+    return () => {
+      window.removeEventListener('message', handleMessage);
+    };
+  }, []);
+
+  useEffect(() => {
+    function handleMutation(mutationsList, observer) {
+      const height = document.documentElement.scrollHeight;
+      if (height !== iframeHeight) {
+        setIframeHeight(height);
+        window.parent.postMessage({type: 'resizeIframe', height}, '*');
+      }
+    }
+
+    const observer = new MutationObserver(handleMutation);
+    const observerConfig = {
+      attributes: true,
+      childList: true,
+      subtree: true,
+      characterData: true,
+    };
+
+    observer.observe(document.body, observerConfig);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [iframeHeight]);
   return (
-    <iframe
-      src={url}
-      title="Embedded Content"
-      frameBorder="0"
-      allowFullScreen
-      ref={iframeRef}
-      width={'100%'}
-      onLoad={() => {
-        iframeRef.current.style.height =
-          iframeRef.current.contentWindow.document.body.scrollHeight + 'px';
-      }}
-    />
+    <div>
+      <iframe
+        src={url}
+        title="Embedded Content"
+        frameBorder="0"
+        allowFullScreen
+        ref={iFrameRef}
+        id="iFrame1"
+        width={'100%'}
+        style={{height: iframeHeight}}
+      />
+    </div>
   );
 }
