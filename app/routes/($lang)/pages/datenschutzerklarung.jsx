b8a30a4139942b6aeb1963083f739da9c1a535e1
Ravipatelbg <114903445+Ravipatelbg@users.noreply.github.com>
2023-06-14 14:47:25 +0530

    Push the code for Impressum,Datenschutzerklärung  page

--- app/routes/($lang)/pages/datenschutzerklarung.jsx
+++ app/routes/($lang)/pages/datenschutzerklarung.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function impressum() {
+export default function datenschutzerklarung() {
   return (
     <>
       <section className="rich-text-section pb-[40px] md:pb-[60px] lg:pb-[80px] xl:pb-[100px]">
@@ -9,7 +9,7 @@ export default function impressum() {
             <div className="col-inner">
               <div className="title-wrap">
                 <h1 className="text-[#00795C] text-[35px] lg:text-[40px] xl:text-[50px] tracking-[-1.05984px] mb-[30px] 2xl:mb-[65px] font-bold">
-                  Impressum
+                Datenschutzerklärung
                 </h1>
               </div>
               <div className="desc text-black text-[16px] lg:text-[21px] leading-[1.3] font-[400]">
