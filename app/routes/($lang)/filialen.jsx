5a1ee18bead4f47e9cb3adefed4cb7760a2f41cf
sagar-brightness <s.chandresha@brightness-india.com>
2023-09-06 15:31:59 +0530

    changed iframe urll to live storelocator

--- app/routes/($lang)/filialen.jsx
+++ app/routes/($lang)/filialen.jsx
@@ -1,31 +1,30 @@
-import { json } from '@shopify/remix-oxygen';
-import { useLoaderData } from '@remix-run/react';
-import {  ResponsiveIframe } from '~/components';
-import { toHTML, getBreadCrumbs} from '~/lib/utils';
-import {Link, Breadcrumb } from '~/components';
-import { AICO_STOREFINDER_URL } from '~/lib/const';
+import {json} from '@shopify/remix-oxygen';
+import {useLoaderData} from '@remix-run/react';
+import {ResponsiveIframe} from '~/components';
+import {toHTML, getBreadCrumbs} from '~/lib/utils';
+import {Link, Breadcrumb} from '~/components';
+import {AICO_STOREFINDER_URL} from '~/lib/const';
 
 const seo = ({data}) => ({
   title: data?.page?.seo?.title,
   description: data?.page?.seo?.description,
-  url : data?.url,
+  url: data?.url,
 });
- 
- export const handle = {
-   seo,
- };
 
-export async function loader({ params, request, context }) {
-    
+export const handle = {
+  seo,
+};
+
+export async function loader({params, request, context}) {
   const {page} = await context.storefront.query(PAGE_QUERY, {
-      variables: {
-        handle: 'filialen',
-        language: context.storefront.i18n.language,
-      },
-    });
+    variables: {
+      handle: 'filialen',
+      language: context.storefront.i18n.language,
+    },
+  });
 
-     return json(
-    {page, url: request.url, locale : context.storefront.i18n.language},
+  return json(
+    {page, url: request.url, locale: context.storefront.i18n.language},
     {
       headers: {
         // TODO cacheLong()
@@ -37,12 +36,13 @@ export async function loader({ params, request, context }) {
 export default function shopfinder() {
   const {locale} = useLoaderData();
 
-   return (
-      <>
-         <Breadcrumb crumbs={getBreadCrumbs(null,'shopfinder')}/>
-         <ResponsiveIframe url={`${AICO_STOREFINDER_URL}?language_code=${locale?.toLocaleLowerCase()}`} />
-      </>
-   );
+  return (
+    <>
+      <Breadcrumb crumbs={getBreadCrumbs(null, 'shopfinder')} />
+      {/* <ResponsiveIframe url={`${AICO_STOREFINDER_URL}?language_code=${locale?.toLocaleLowerCase()}`} /> */}
+      <ResponsiveIframe url={`${AICO_STOREFINDER_URL}`} />
+    </>
+  );
 }
 
 const PAGE_QUERY = `#graphql
