317e487d35da350e142676d2b088dbe71e668008
BrunoDeLu <bruno.delucca@kybunjoya.swiss>
2024-11-05 09:35:59 +0100

    Update Formular Dr. kybunjoya

--- app/components/klaviyo_form.jsx
+++ app/components/klaviyo_form.jsx
@@ -22,8 +22,6 @@ export function Klaviyo_form({klaviyo_listID,set_language}) {
     loading_button_text= "Processing..."
     successful_header_text = "Thank you for your interest in the kybun Joya therapy brochure."
     successful_message = "We will send you the link to download the therapy brochure to your e-mail address."
-    unsuccessful_header_text = "Thanks for your submition."
-    unsuccessful_message = "A free copy of this book has already been sent to this Email. If you haven't receive any email, please contact us"
   } else {
     form_email = "E-mail"
     form_name = "Vorname"
@@ -36,12 +34,9 @@ export function Klaviyo_form({klaviyo_listID,set_language}) {
     loading_button_text= "Bearbeitung..."
     successful_header_text = "Vielen Dank für Ihr Interesse an der kybun Joya Therapiebroschüre."
     successful_message = "Wir senden Ihnen den Link zum Download der Therapiebroschüre per E-Mail zu."
-    unsuccessful_header_text = "Vielen Dank für Ihr Interesse an der kybun Joya Therapiebroschüre."
-    unsuccessful_message = "Ein kostenloses Exemplar dieses Buches wurde bereits an diese E-Mail gesendet. Wenn Sie keine E-Mail erhalten haben, kontaktieren Sie uns bitte"
   }
-  const [isActive, showLoading] = useState(true);
+  const [isLoading, showLoading] = useState(true);
   const [submited, showEndMessage] = useState(true);
-  const [registered, showEndMessage2] = useState(true);
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
@@ -51,115 +46,32 @@ export function Klaviyo_form({klaviyo_listID,set_language}) {
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    showLoading(!isActive);
+    showLoading(!isLoading);
     const get_options_klaviyo = {
       method: 'GET',
       headers: {
-        accept: 'application/json',
-        revision: '2024-07-15',
-        Authorization: 'Klaviyo-API-Key pk_2b2617e9aec18a60007bf77b72b4999f6d'
+        accept: 'application/vnd.api+json',
+        revision: '2024-10-15',
+        'content-type': 'application/vnd.api+json'
       }
     };
-    
-    fetch('https://a.klaviyo.com/api/profiles/?filter=equals(email,"'+formData.email+'")&page[size]=5', get_options_klaviyo)
-      .then(response => response.json())
-      .then(response => {
-        /// OPTION 1 CUSTOMER NOT IN KLAVIYO DATABASE
-        if(response.data.length == 0){ // email not in Klaviyo
-          const post_profile_klaviyo = {
-            method: 'POST',
-            headers: {
-              accept: 'application/json',
-              revision: '2024-07-15',
-              'content-type': 'application/json',
-              Authorization: 'Klaviyo-API-Key pk_2b2617e9aec18a60007bf77b72b4999f6d'
-            },
-            body: JSON.stringify({
-              data: {
-                type: 'profile',
-                attributes: {
-                  email: formData.email,
-                  first_name: formData.firstName,
-                  last_name: formData.lastName
-                }
-              }
-            })
-          };
-          fetch('https://a.klaviyo.com/api/profiles/', post_profile_klaviyo)
-          .then(response => response.json())
-          .then(response => {
-            const post_profileInList_klaviyo = {
-              method: 'POST',
-              headers: {
-                accept: 'application/json',
-                revision: '2024-07-15',
-                'content-type': 'application/json',
-                Authorization: 'Klaviyo-API-Key pk_2b2617e9aec18a60007bf77b72b4999f6d'
-              },
-              body: JSON.stringify({data: [{type: 'profile', id: response.data.id}]})
-            };
-            fetch('https://a.klaviyo.com/api/lists/'+listID+'/relationships/profiles/', post_profileInList_klaviyo)
-              .then(response => {
-                removeElement("dr_kybunjoya_form");
-                showEndMessage(!submited)
-              })
-              .catch(err => console.error(err));
-          })
-          .catch(err => console.error(err));
-        /// OPTION 2 CUSTOMER IN KLAVIYO DATABASE
-        } else {
-          const user = response.data
-          const search_profile_list = {
-            method: 'GET',
-            headers: {
-              accept: 'application/json',
-              revision: '2024-07-15',
-              Authorization: 'Klaviyo-API-Key pk_2b2617e9aec18a60007bf77b72b4999f6d'
-            }
-          };
-          fetch('https://a.klaviyo.com/api/lists/'+listID+'/profiles/?filter=equals(email,"'+formData.email+'")&page[size]=5', search_profile_list)
-            .then(response => response.json())
-            .then(response => {
-              /// OPTION 2.1 CUSTOMER IN LIST DATABASE
-              if(response.data.length != 0){
-                removeElement("dr_kybunjoya_form");
-                showEndMessage2(!registered)
-              /// OPTION 2.2 CUSTOMER NOT IN LIST DATABASE
-              } else {
-                const post_profileInList_klaviyo = {
-                  method: 'POST',
-                  headers: {
-                    accept: 'application/json',
-                    revision: '2024-07-15',
-                    'content-type': 'application/json',
-                    Authorization: 'Klaviyo-API-Key pk_2b2617e9aec18a60007bf77b72b4999f6d'
-                  },
-                  body: '{"data":[{"type":"profile","id":"'+user[0].id+'"}]}'
-                };
-                fetch('https://a.klaviyo.com/api/lists/'+listID+'/relationships/profiles', post_profileInList_klaviyo)
-                  .then(response => response)
-                  .then(response => {
-                      removeElement("dr_kybunjoya_form");
-                      showEndMessage(!submited)
-                    }
-                  )
-                  .catch(err => console.error(err));
-              }
-            })
-            .catch(err => console.error(err));
-        }
-      })
+
+    const options = {
+      method: 'POST',
+      headers: {revision: '2024-10-15', 'content-type': 'application/vnd.api+json'},
+      body: '{"data":{"type":"subscription","attributes":{"profile":{"data":{"type":"profile","attributes":{"properties":{"Language":"'+formData.language+'"},"email":"'+formData.email+'","first_name":"'+formData.firstName+'","last_name":"'+formData.lastName+'"}}},"custom_source":"sub_form_website"},"relationships":{"list":{"data":{"type":"list","id":"'+listID+'"}}}}}'
+    };
+    fetch('https://a.klaviyo.com/client/subscriptions?company_id=Xpi5VF', options)
+    .then(res => {
+      removeElement("dr_kybunjoya_form");
+      showEndMessage(!submited)
+    })
       .catch(err => console.error(err));
   };
-
-  useEffect(() => {
-
-  }, []);
   return (
     <>
-
       <form id='dr_kybunjoya_form' onSubmit={handleSubmit} className='relative transition-all ease-in'>
-        <div id="loading_request_animation" className={`absolute left-2/4 top-1/3 transform_loading_magic ${isActive ? 'hidden' : ''}`}>
+        <div id="loading_request_animation" className={`absolute left-2/4 top-1/3 transform_loading_magic ${isLoading ? 'hidden' : ''}`}>
           <div className="flex items-center justify-center">
             <button type="button" className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-emerald-900 hover:bg-emerald-800 transition ease-in-out duration-150 cursor-not-allowed" disabled="">
               <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
@@ -211,8 +123,8 @@ export function Klaviyo_form({klaviyo_listID,set_language}) {
             <option value="" disabled selected hidden>
               {form_language}
             </option>
-            <option value="English">{form_english_option}</option>
-            <option value="German">{form_german_option}</option>
+            <option value="EN">{form_english_option}</option>
+            <option value="DE">{form_german_option}</option>
           </select>
         </div>
 
@@ -222,10 +134,6 @@ export function Klaviyo_form({klaviyo_listID,set_language}) {
       <div className={`pt-3.5 pb-3.5 pr-3.5 pl-3.5 bg-emerald-600 text-white rounded-xl	 ${submited ? 'hidden' : ''}`} style={{backgroundColor: '#00795c'}}>
         <p className='text-center text-2xl font-bold'>{successful_header_text}</p>
         <p className='text-center text-lg'>{successful_message}</p>
-      </div>
-      <div className={`pt-3.5 pb-3.5 pr-3.5 pl-3.5 bg-emerald-600 text-white rounded-xl ${registered ? 'hidden' : ''}`} style={{backgroundColor: '#00795c'}}>
-        <p className='text-center text-2xl font-bold'>{unsuccessful_header_text}</p>
-        <p className='text-center text-lg'>{unsuccessful_message}</p>
       </div>
     </>
   );
